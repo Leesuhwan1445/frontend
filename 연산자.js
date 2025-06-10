dfd33cd6b08e79733caeb738826a4c70e26568cb
이수환 <leesuhwan@isuhwan-ui-MacBookAir.local>
2025-06-10 13:14:10 +0900

    javascript 공부

---
+++ 연산자.js
@@ -0,0 +1,26 @@
+const a = 1;
+const b = '1';
+const c = 2;
+const d = 1;
+
+// 비교 연산자
+
+console.log('a == b ', a == b);
+console.log('a === b', a === b);
+console.log(' a > c ', a < c)
+console.log('a < c', a <= c);
+
+//증감 연산자
+let e = 1;
+e++; // e = e +1;
+console.log('e++', e);
+e--;
+console.log('e--', e_);
+console.log('++e', ++e);
+console.log('--e', --e);
+
+console.log('a === b && a < c', a === b && a < c); // 둘중 하나라도 false 면 false === < 값과 타입
+console.log('a === b || a < c', a === b || a < c); // 둘중 하나라도 true 면 true 
+console.log(!false);
+console.log('a !==b', a !== b)
+console.log('a !=b', a != b)
---
+++ 함수.js
@@ -0,0 +1,17 @@
+const a = 1;
+const b = 10;
+const c = 100;
+
+function funtionname(x, y) {
+    return x + y;
+}
+
+console.log(funtionname(a, b))
+
+//화살표 함수
+
+const arrowfuntion = (x, y) => {
+    return x + y
+}
+console.log(funtionname(a - c))
+
